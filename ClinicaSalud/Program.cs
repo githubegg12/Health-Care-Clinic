8cecc7070b539a4c7cffc3596507e3dd1b58817c
githubegg12 <davidvargas1224@gmail.com>
2025-09-25 17:11:45 -0500

    Task 3, Defining the Patient class

--- ClinicaSalud/Program.cs
+++ ClinicaSalud/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hola Clínica Salud+");
+
+using ClinicaSalud.Models;
+
+// Creating patients
+Patient patient= new Patient();
+patient.Name = "Juan";
+patient.Lastname = "Pérez";
+patient.Age = 30;
+patient.Id = 12345678;
+patient.Symptom = "I have Cancer";
+Console.WriteLine($"Paciente 1: {patient.Name} {patient.Lastname}");
+
