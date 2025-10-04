ed0564d7e1be315cdff1d1bf7b5b30d7d02effeb
githubegg12 <davidvargas1224@gmail.com>
2025-10-03 19:39:15 -0500

    Task 2, Defining classes based on UML

--- ClinicaSalud/Models/Pet.cs
+++ ClinicaSalud/Models/Pet.cs
@@ -2,6 +2,7 @@ namespace ClinicaSalud.Models;
 
 public class Pet(string namePet, string species, int agePet, string breed )
 {
+    
     public string NamePet { get; set; } = namePet;
     public string Species { get; set; } = species;
     public int AgePet { get; set; } = agePet;
